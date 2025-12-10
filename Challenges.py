5c60e20eba5b489e5a01875a094c24f8fcd4e511
Senorpancake2 <Jadenhasenfus557@gmail.com>
2025-12-10 14:20:17 +0000

    second challenge

--- Challenges.py
+++ Challenges.py
@@ -1,11 +1,26 @@
 print()
 
+#Challenge 1
 
-hours = int(input("How many hours did Denise sleep:"))
+hours = int(input("How many hours did Denise work:"))
 
 if hours >= 48:
     print("Not Possible")
 elif hours >= 24:
-    print(hours-24)
+    print("Hours slept:", hours-24)
 else:
-    print(24-hours)
+    print("Hours slept:", 24-hours)
+
+    #Challenge 2
+
+score=int(input("Enter score: "))
+
+wins = score%42
+
+ties = wins%10
+
+print(ties)
+
+
+#Challenge 3
+
